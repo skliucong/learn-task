82ffb2bf4d5aa9f38ef7b6303ad9241d23c34d63
skliucong <skliucong@gmail.com>
2019-01-10 20:16:54 +0800

    first

--- leetcode/func17.py
+++ leetcode/func17.py
@@ -1,10 +1,33 @@
 class Solution:
+    ll=[]
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
+        if digits=="":
+            return []
         ma={2:['a','b','c'],3:['d','e','f'],4:['g','h','i'],5:['j','k','l'],6:['m','n','o'],7:['p','q','r','s'],8:['t','u','v'],9:['w','x','y','z']}
+        lis=[]
         for  item in digits:
             ite=int(item)
-            print(ite)
+            lis.append(ma.get(ite))
+        self.ll=[]
+        self.combin("",lis)
+        return self.ll
+
+    def combin(self,pailie,lis):
+        if len(lis)==1:
+            for item in lis[0]:
+                self.ll.append(pailie+item)
+        else:
+            for item in lis[0]:
+                self.combin(pailie+item,lis[1:])
+
+
+
+
+
+if __name__ == '__main__':
+    re=Solution().letterCombinations("23")
+    print(re)
