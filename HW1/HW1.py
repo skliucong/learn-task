4c175b2afc8de1a495168c397b0c5182d4db03fc
skliucong <skliucong@gmail.com>
2019-01-10 15:39:35 +0800

    first

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -4,25 +4,7 @@ def MaxMinNormalization(x,Max,Min):
         return 0
     x = (x - Min) / (Max - Min)
     return x
-
-def getset():
-    file=open("test.csv",'r')
-    file2 = open("ans.csv", 'r')
-
-    rea=file.readlines()
-    total=[]
-    for i in range(0,len(rea),18):
-        total.append(rea[i:i+18])
-    # print(total)
-    file.close()
-
-    rea2=file2.readlines()
-    label = [item.strip('\n').split(',')[-1] for item in rea2[1:]]
-    file2.close()
-
-
-    x=[]
-
+def GetMaxMin(total):
     ma=[]
     mi=[]
     for item in total:
@@ -32,21 +14,35 @@ def getset():
                 mi.append(0)
                 continue
             item[i]=[float(iii) for iii in item[i].strip('\n').split(',')[2:]]
-
             if len(ma)<=i:
                 ma.append(np.max(item[i]))
             else:
                 if np.max(item[i])>ma[i]:
                     ma[i]=np.max(item[i])
-
-
             if len(mi)<=i:
                 mi.append(np.min(item[i]))
             else:
                 if np.min(item[i])<mi[i]:
                     mi[i]=np.min(item[i])
+    return ma,mi
+
+def getset():
+    trainfile=open("test.csv",'r')
+    labelfile = open("ans.csv", 'r')
+
+    rea=trainfile.readlines()
+    total=[]
+    for i in range(0,len(rea),18):
+        total.append(rea[i:i+18])
+    trainfile.close()
+
+    rea2=labelfile.readlines()
+    label = [item.strip('\n').split(',')[-1] for item in rea2[1:]]
+    labelfile.close()
 
 
+    x=[]
+    ma,mi=GetMaxMin(total)
 
     for item in total:
         it=[]
@@ -57,44 +53,29 @@ def getset():
                 it=it+[MaxMinNormalization(itemits,ma[i],mi[i]) for itemits in  its]
         x.append(it)
     return np.array(x,dtype = float),np.array(label,dtype = float)
-# b=np.random.rand()
-#w=np.random.rand(153)
-w=A=np.zeros(153)
-x,label=getset()
-print(x.shape)
-learn_rate=0.0001
-y=np.dot(x,w)
-print(y.shape)
-loss=sum(pow(label-y,2)/2)
-
-for i in range(1000000):
-    v=np.dot((label-y),x)
-
-    w=w+learn_rate*v
-    y = np.dot(x, w)
-    loss = sum(pow(label - y, 2) / 2)/len(y)
-    if i%10000==0:
-        print(loss)
 
 
+if __name__ == '__main__':
 
-#############求导
+    learn_rate=0.0001
+    epoch=1000000
 
+    x,label=getset()
+    w=np.random.rand(len(x[0]))
 
+    y=np.dot(x,w)
+    loss=sum(pow(label-y,2)/2)
 
+    for i in range(epoch):
 
+        v=np.dot((label-y),x)
+        w=w+learn_rate*v
 
+        y = np.dot(x, w)
 
-
-
-
-
-
-
-
-# y=w*x+b
-#loss=(label-y)^2
-
+        loss = sum(pow(label - y, 2) / 2)/len(label)
+        if i%10000==0:
+            print(loss)
 
 
 
